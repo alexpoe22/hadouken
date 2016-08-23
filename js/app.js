70bd50b4fa8dd2b7c20f1f1c53ae67f9fa1008c2
alexpoe22 <DRIPS@109s-mbp-3.pk5001z>
2016-08-23 14:58:13 -0700

    songfixed

--- js/app.js
+++ js/app.js
@@ -22,8 +22,9 @@ $(document).ready(function() {
             $('.ryu-ready').show();
             $('.ryu-still').hide();
         })
-    $(document).keydown(function(event) {
+    $("body").keydown(function(event) {
     if (event.which == 88) {
+        event.preventDefault()
         playHard();
 
             $('.ryu-still').hide();
@@ -32,8 +33,8 @@ $(document).ready(function() {
             $('.ryu-cool').show();
         }
     });
-    $(document).keyup(function(event) {
-       
+    $("body").keyup(function(event) {
+        $('#cool')[0].pause();
         $('.ryu-still').show();
         $('.ryu-ready').hide();
         $('.ryu-throwing').hide();
@@ -43,17 +44,35 @@ $(document).ready(function() {
     });
     
 
+    $('#cool')[0].volume = 0.5;
+    $('#cool')[0].load();
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
 });
 
 function playHadouken() {
-    $('#hadouken-sound')[0].volume = 0.5;
-    $('#hadouken-sound')[0].load();
     $('#hadouken-sound')[0].play();
 }
 
 function playHard() {
-    $('#cool')[0].volume = 0.5;
-    $('#cool')[0].load();
     $('#cool')[0].play();
 }
+
+// function doIntro(){
+//     $('#theme-song')[0].volume=0.3;
+//     $('#theme-song')[0].play();
+//     $('.sf-logo').fadeIn(3500, function() {
+//         $('.sf-logo').fadeOut(1000, function(){
+//             $('.brought-by').fadeIn(1500, function(){
+//                 $('.brought-by').fadeOut(1000, function(){
+//                     $('.jquery-logo').fadeIn(1500, function(){
+//                         $('.jquery-logo').fadeOut(1000, function(){
+//                             $('.how-to').fadeIn(1000);
+//                         });
+//                     });
+//                   });
+//                });
+//             });
+//          }); 
+// }          
 
